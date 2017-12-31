93867b05094060e2365dc2eaa8dae57b5f64da06
KyStyle67 <pkyle167@gmail.com>
2017-12-31 21:09:44 +1100

    Added Version 1 of my Cookie Clicker Game
    V1 implements the cookie, the clicking, the upgrades, still will be working on monitor implementation and maybe a server leaderboard

---
+++ cclicker.lua
@@ -0,0 +1,342 @@
+-- COOKIE CLICKERS, HELL YEAH!
+local w, h = term.getSize() -- First getting the size of the computer screen display
+
+cookies = 0
+cps = 0
+
+local function drawMenuColour() -- creates the colours of the menu
+    paintutils.drawFilledBox(0, 0, w, h, colours.grey) -- Bottom Grey
+    paintutils.drawFilledBox(0, 0, w, 2, colours.orange) -- top 2 Cyan
+end
+
+local function drawTitle()
+    term.setCursorPos(17,1)
+    term.setTextColour(colours.black)
+    term.setBackgroundColour(colours.orange)
+    print("COOKIE CLICKERS - V1")     --Title
+    print(string.rep("=", w))
+    term.setBackgroundColour(colours.lightGrey)
+end
+
+local function drawHeadings() -- creates the game statistic displays
+    term.setBackgroundColour(colours.grey)
+    term.setTextColour(colours.lightGrey)
+    paintutils.drawLine(2,4,27,4, colours.grey)
+    term.setCursorPos(2, 4)
+    print("Cookies:", cookies)
+    paintutils.drawLine(2,5,27,5, colours.grey)
+    term.setCursorPos(2,5)
+    print("CPS:", cps)
+end
+
+local function drawCookie()
+  paintutils.drawFilledBox(4,7,13,13, colours.orange)
+  paintutils.drawPixel(4,7, colours.grey)
+  paintutils.drawPixel(13,7, colours.grey)
+  paintutils.drawPixel(4,13, colours.grey)
+  paintutils.drawPixel(13,13, colours.grey)
+  paintutils.drawPixel(5,9, colours.black)
+  paintutils.drawPixel(7,12, colours.black) -- W,H,Colour
+  paintutils.drawPixel(10,10, colours.black)
+  paintutils.drawPixel(8,8, colours.black)
+  paintutils.drawPixel(11,12, colours.black)
+  paintutils.drawPixel(12,9, colours.black)
+end
+
+local function drawExit()
+  term.setBackgroundColor(colours.red)
+  term.setTextColor(colours.white)
+  term.setCursorPos(2, h-1)
+  print("EXIT")
+end
+
+local function exitPrompt()
+--making fancy boxes
+  paintutils.drawFilledBox(18,6,42,14, colours.black)
+  paintutils.drawFilledBox(18,6,42,7, colours.orange)
+  --Making a fancy title
+  term.setBackgroundColor(colours.orange)
+  term.setTextColor(colours.black)
+  term.setCursorPos(26,6)
+  print("EXIT MENU")
+  term.setCursorPos(18,7)
+  print(string.rep("=", 25))
+  --making fancy text
+  term.setBackgroundColor(colours.black)
+  term.setTextColor(colours.white)
+  term.setCursorPos(20, 9)
+  print("Are you sure you want")
+  term.setCursorPos(26,10)
+  print("to exit?")
+  --Making fancy Yes/No boxes
+  term.setBackgroundColor(colours.green)
+  term.setCursorPos(26, 12)
+  print("YES")
+  term.setBackgroundColor(colours.red)
+  term.setCursorPos(32, 12)
+  print("NO ")
+  -- using the exit GUI
+  while true do
+    local event, button, x, y = os.pullEventRaw("mouse_click") -- It enables you to select with the mouse
+    if (event == "mouse_click" and x >= 18  and 42 >= x and y >= 6  and 14 >= y) then -- if you click in the window zone
+      if (event == "mouse_click" and x >= 26 and 28 >= x and y == 12) then -- and if you also click on the 'YES'
+        term.setTextColor(colours.yellow)
+        term.setBackgroundColor(colours.black)
+        term.setCursorPos(26, 12)
+        textutils.slowPrint(" Goodbye!")
+        sleep(1)
+        return true
+      elseif (x >= 32 and 35 >= x and y == 12) then -- and if you also click on the 'NO'
+        return false
+      end
+    elseif button == 28 then -- If you hit enter
+      term.setTextColor(colours.yellow)
+      term.setBackgroundColor(colours.black)
+      term.setCursorPos(26, 12)
+      textutils.slowPrint(" Goodbye!")
+      sleep(1)
+      return true
+    else
+      return false
+    end
+  end
+end
+
+--The upgrade menu panel
+
+pickaxeAmount = 0
+pickaxePrice = 25
+villagerAmount = 0
+villagerPrice = 75
+farmAmount = 0
+farmPrice = 300
+mineAmount = 0
+minePrice = 1000
+factoryAmount = 0
+factoryPrice = 7500
+portalAmount = 0
+portalPrice = 25000
+
+local function drawUpgrades()
+  --PICKAXE Upgrade menu
+  if cookies >= pickaxePrice then -- If you have enough money, it's green
+    paintutils.drawFilledBox(w-23, 4, w-13, 7, colours.green)
+    term.setTextColor(colours.black)
+    term.setBackgroundColor(colours.green)
+  else -- If you don't have enough money, it's red
+    paintutils.drawFilledBox(w-23, 4, w-13, 7, colours.red)
+    term.setTextColor(colours.white)
+    term.setBackgroundColor(colours.red)
+  end
+  -- Writing the text
+  term.setCursorPos(w-22,4)
+  print("Pickaxe")
+  term.setCursorPos(w-22,5)
+  print("0.3 CPS")
+  term.setCursorPos(w-22,6)
+  print("$",pickaxePrice)
+  term.setCursorPos(w-22,7)
+  print("Own: ",pickaxeAmount)
+
+  --VILLAGER upgrade menu
+  if cookies >= villagerPrice then -- If you have enough money, it's green
+    paintutils.drawFilledBox(w-11, 4, w-1, 7, colours.green)
+    term.setTextColor(colours.black)
+    term.setBackgroundColor(colours.green)
+  else -- If you don't have enough money, it's red
+    paintutils.drawFilledBox(w-11, 4, w-1, 7, colours.red)
+    term.setTextColor(colours.white)
+    term.setBackgroundColor(colours.red)
+  end
+  --Writing the text
+  term.setCursorPos(w-10,4)
+  print("Villager")
+  term.setCursorPos(w-10,5)
+  print("1 CPS")
+  term.setCursorPos(w-10,6)
+  print("$",villagerPrice)
+  term.setCursorPos(w-10,7)
+  print("Own: ",villagerAmount)
+
+  -- FARM upgrade menu
+  if cookies >= farmPrice then -- If you have enough money, it's green
+    paintutils.drawFilledBox(w-23, 9, w-13, 12, colours.green)
+    term.setTextColor(colours.black)
+    term.setBackgroundColor(colours.green)
+  else -- If you don't have enough money, it's red
+    paintutils.drawFilledBox(w-23, 9, w-13, 12, colours.red)
+    term.setTextColor(colours.white)
+    term.setBackgroundColor(colours.red)
+  end
+  --Writing the text
+  term.setCursorPos(w-22,9)
+  print("Farm")
+  term.setCursorPos(w-22,10)
+  print("3 CPS")
+  term.setCursorPos(w-22,11)
+  print("$",farmPrice)
+  term.setCursorPos(w-22,12)
+  print("Own: ",farmAmount)
+
+  --Mine Upgrade Menu
+  if cookies >= minePrice then -- If you have enough money, it's green
+    paintutils.drawFilledBox(w-11, 9, w-1, 12, colours.green)
+    term.setTextColor(colours.black)
+    term.setBackgroundColor(colours.green)
+  else -- If you don't have enough money, it's red
+    paintutils.drawFilledBox(w-11, 9, w-1, 12, colours.red)
+    term.setTextColor(colours.white)
+    term.setBackgroundColor(colours.red)
+  end
+  --Writing the text
+  term.setCursorPos(w-10,9)
+  print("Mine")
+  term.setCursorPos(w-10,10)
+  print("5 CPS")
+  term.setCursorPos(w-10,11)
+  print("$",minePrice)
+  term.setCursorPos(w-10,12)
+  print("Own: ",mineAmount)
+
+  --Factory Upgrade Menu
+  if cookies >= factoryPrice then -- If you have enough money, it's green
+    paintutils.drawFilledBox(w-23, 14, w-13, 17, colours.green)
+    term.setTextColor(colours.black)
+    term.setBackgroundColor(colours.green)
+  else -- If you don't have enough money, it's red
+    paintutils.drawFilledBox(w-23, 14, w-13, 17, colours.red)
+    term.setTextColor(colours.white)
+    term.setBackgroundColor(colours.red)
+  end
+  --Writing the text
+  term.setCursorPos(w-22,14)
+  print("Factory")
+  term.setCursorPos(w-22,15)
+  print("15 CPS")
+  term.setCursorPos(w-22,16)
+  print("$",factoryPrice)
+  term.setCursorPos(w-22,17)
+  print("Own: ",factoryAmount)
+
+  --Portal Upgrade Menu
+  if cookies >= portalPrice then -- If you have enough money, it's green
+    paintutils.drawFilledBox(w-11, 14, w-1, 17, colours.green)
+    term.setTextColor(colours.black)
+    term.setBackgroundColor(colours.green)
+  else -- If you don't have enough money, it's red
+    paintutils.drawFilledBox(w-11, 14, w-1, 17, colours.red)
+    term.setTextColor(colours.white)
+    term.setBackgroundColor(colours.red)
+  end
+  --Writing the text
+  term.setCursorPos(w-10,14)
+  print("Portal")
+  term.setCursorPos(w-10,15)
+  print("50 CPS")
+  term.setCursorPos(w-10,16)
+  print("$",portalPrice)
+  term.setCursorPos(w-10,17)
+  print("Own: ",portalAmount)
+end
+
+-- Functions to run the game
+
+local function runGame()
+  while true do -- The continuous loop of the game
+    drawHeadings() -- draws in all the data
+    local event, button, x, y = os.pullEventRaw() -- if there is a mouse click
+    if (event == "mouse_click" and x >= 4  and 13 >= x and y >= 7  and 13 >= y) then -- and if that mouse click is on the cookie
+      cookies = cookies + 1 -- you get more cookies
+    end
+    if (event == "mouse_click" and 5 >= x and x >= 2 and y == h-1) then -- If you hit EXIT
+      return
+    end
+    if (event == "mouse_click" and x >= 28 and 38 >= x and y >= 4 and 7 >= y) then -- PICKAXE CLICK
+      if cookies >= pickaxePrice then
+        cookies = cookies - pickaxePrice
+        pickaxeAmount = pickaxeAmount + 1
+      end
+    end
+    if (event == "mouse_click" and x >= 40 and 50 >= x and y >= 4 and 7 >= y) then -- VILLAGER CLICK
+      if cookies >= villagerPrice then
+        cookies = cookies - villagerPrice
+        villagerAmount = villagerAmount + 1
+      end
+    end
+    if (event == "mouse_click" and x >= 28 and 38 >= x and y >= 9 and 12 >= y) then -- FARM CLICK
+      if cookies >= villagerPrice then
+        cookies = cookies - farmPrice
+        farmAmount = farmAmount + 1
+      end
+    end
+    if (event == "mouse_click" and x >= 40 and 50 >= x and y >= 9 and 12 >= y) then -- MINE CLICK
+      if cookies >= minePrice then
+        cookies = cookies - minePrice
+        mineAmount = mineAmount + 1
+      end
+    end
+    if (event == "mouse_click" and x >= 28 and 38 >= x and y >= 14 and 17 >= y) then -- FACTORY CLICK
+      if cookies >= factoryPrice then
+        cookies = cookies - factoryPrice
+        factoryAmount = factoryAmount + 1
+      end
+    end
+    if (event == "mouse_click" and x >= 40 and 50 >= x and y >= 14  and 17 >= y) then -- PORTAL CLICK
+      if cookies >= portalPrice then
+        cookies = cookies - portalPrice
+        portalAmount = portalAmount + 1
+      end
+    end
+    if event == "terminate" then
+      break
+    end
+    cps = (pickaxeAmount*0.3) + (villagerAmount*1) + (farmAmount*3) + (mineAmount*5) + (factoryAmount*15) + (portalAmount*50)
+    pickaxePrice = 25 + (pickaxeAmount*5)
+    villagerPrice = 75 + (villagerAmount*20)
+    farmPrice = 300 + (farmAmount*75)
+    minePrice = 1000 + (mineAmount*350)
+    factoryPrice = 7500 + (factoryAmount*1200)
+    portalPrice = 25000 + (portalAmount*10000)
+    drawUpgrades()
+  end
+end
+
+local function runUpgrades()
+  while true do -- continuously adding cookies every amount of seconds for the different runUpgrades
+    sleep(1)
+    cps = (pickaxeAmount*0.3) + (villagerAmount*1) + (farmAmount*3) + (mineAmount*5) + (factoryAmount*15) + (portalAmount*50)
+    cookies = cookies + cps
+    drawHeadings() -- draws in all the data
+    drawUpgrades()
+  end
+end
+
+-- Running the game
+
+drawMenuColour()
+drawTitle()
+drawExit()
+drawCookie()
+drawUpgrades()
+
+while true do
+  parallel.waitForAny(runGame, runUpgrades)
+  if exitPrompt() then
+    break
+  else
+    drawMenuColour()
+    drawTitle()
+    drawExit()
+    drawCookie()
+    drawUpgrades()
+  end
+end
+
+--Clears the screen and leaves goodbye splash
+term.setCursorPos(1, 1)
+term.setBackgroundColor(colours.black)
+term.setTextColor(colours.orange)
+term.clear()
+print("Programmed by Kyle Phillips,")
+print("Thanks for playing!")
+term.setTextColour(colours.white)
