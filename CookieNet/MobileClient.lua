9fe6738de3859a769d0d1417f5e4324841165c68
MAK064 <Mattakirk@outlook.com>
2018-01-07 16:14:01 +1100

    Update MobileClient.lua

--- CookieNet/MobileClient.lua
+++ CookieNet/MobileClient.lua
@@ -1,8 +1,15 @@
--- A Pocket Computer is 26 x 20
+local w, h = term.getSize()
+-- A Pocket Computer is 26 x 20 (w x h)
+
 function printCentered(sText)
     local w, h = term.getSize()
     local x, y = term.getCursorPos()
     x = math.max(math.floor((w / 2) - (#sText / 2)), 0)
     term.setCursorPos(x, y)
-    print(sText)
+		print(sText)
 end
+
+local function drawMainMenu()
+		paintutils.drawFilledBox(0, 0, w, 1)
+
+local function drawGPS
