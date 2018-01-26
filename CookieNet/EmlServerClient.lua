8e0581ab57e844da8e1b8c573d7b1a3a181cb038
MAK064 <Mattakirk@outlook.com>
2018-01-26 22:09:57 +1100

    Update EmlServerClient.lua

--- CookieNet/EmlServerClient.lua
+++ CookieNet/EmlServerClient.lua
@@ -5,7 +5,7 @@ local modem = peripheral.wrap("back")
 local m = peripheral.wrap("right")
 local w, h = m.getSize()
 m.setBackgroundColour(colours.grey)
-paintutils.drawLine(0,0,w,h,colours.green)
+paintutils.drawFilledBox(0,0,w,3,colours.green)
 
 modem.open(100)
 
