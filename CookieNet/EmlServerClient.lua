7f393fa71b8b3d77cd5f4bf702621a037c5e7601
MAK064 <Mattakirk@outlook.com>
2018-01-26 21:44:31 +1100

    Update EmlServerClient.lua

--- CookieNet/EmlServerClient.lua
+++ CookieNet/EmlServerClient.lua
@@ -1,7 +1,8 @@
 -- Initial Startup
 os.setComputerLabel("100")
 local modem = peripheral.wrap("back")
-monitor = peripheral.wrap("left")
+local m = peripheral.wrap("right")
+local w, h = m.getSize()
 modem.open(100)
 
 -- Example Code
