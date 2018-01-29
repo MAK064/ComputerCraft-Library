61c0d313b963c25ec27350b71a7ea528011931f8
MAK064 <Mattakirk@outlook.com>
2018-01-29 16:56:52 +1100

    Bugfix

--- CookieNet/EmlServerClient.lua
+++ CookieNet/EmlServerClient.lua
@@ -21,16 +21,17 @@ modem.open(21)
 
 if true do
 local side, frequency, replyFrequency, message = os.pullEvent(modem_message)
-if frequency = 1
-  then modem.transmit(10, replyFrequency, message)
-  modem.transmit(11 replyFrequency, message)
+  if frequency = 1
+    then modem.transmit(10, replyFrequency, message)
+   modem.transmit(11 replyFrequency, message)
   
-  elseif frequency = 2
-  then modem.transmit(20, replyFrequency, message)
-  modem.transmit(21, replyFrequency, message)
+    elseif frequency = 2
+    then modem.transmit(20, replyFrequency, message)
+    modem.transmit(21, replyFrequency, message)
   
-  elseif frequency = 100
-  then m.print(frequency : message)
+    elseif frequency = 100
+    then m.print(frequency : message)
   
-  else modem.transmit(replyFrequency, 100, "The contact you have specified could not be found")
+    else modem.transmit(replyFrequency, 100, "The contact you have specified could not be found")
+  end
 end
